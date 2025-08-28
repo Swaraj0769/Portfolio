cf78cb89f69442a481953680c713f7ff90e82de9
Swaraj0769 <swarajsanodiya6@gmail.com>
2025-08-28 17:13:13 +0530

    stair animation done (refresh)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
 import { Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
 
 const App = () => {
+
   return (
     <div className='text-6xl'>
       {/* <Link to='/'>Home</Link>
       <Link to='/agence'>Agence</Link>
       <Link to='/project'>Projects</Link> */}
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/agence' element={<Agence />} />
